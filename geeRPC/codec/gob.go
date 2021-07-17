cea686aad5ece39922bda69789f2b7ada9bd0821
chuan-L <chuan_l@qq.com>
2021-07-17 17:33:41 +0000

    geeRPC day0

---
+++ geeRPC/codec/gob.go
@@ -0,0 +1,26 @@
+package codec
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+)
+
+type GobCodec struct {
+	conn io.ReadWriteCloser
+	buf  *bufio.Writer
+	dec  *gob.Decoder
+	enc  *gob.Encoder
+}
+
+var _ Codec = (*GobCodec)(nil)
+
+func NewGobCodec(conn io.ReadWriteCloser) Codec {
+	buf := bufio.NewWriter(conn)
+	return &GobCodec{
+		conn: conn,
+		buf:  buf,
+		dec:  gob.NewDecoder(conn),
+		enc:  gob.NewEncoder(buf),
+	}
+}
